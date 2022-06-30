79151329dc1288dcfcae585957ec4778adf970fb
Uko Udo <donnywise.uk@gmail.com>
2022-06-30 14:00:23 -0700

    pn4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/* main = the entry point
- * return 0 after printing the function
+/**
+ *  main = the entry point
+ *
+ * return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
