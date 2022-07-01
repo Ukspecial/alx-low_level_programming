36cefed7ed4fc20ce0d5a7c2c380115bbd975874
Uko Udo <donnywise.uk@gmail.com>
2022-07-01 13:14:08 -0700

    new addition

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	for (int x = 'a'; x <= 'z'; x++)
+	{
+	    putchar (" %c\n", a);
+	}
+	return (0);
+
+}
