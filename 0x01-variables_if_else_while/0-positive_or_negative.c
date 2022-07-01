77863139b00c9888269a9b6cefa95e273a0e5866
Uko Udo <donnywise.uk@gmail.com>
2022-07-01 09:34:50 -0700

    testing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,9 @@
-#include <stdlib.h
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-/* main - Entry point */                                            
-/* Return: Always 1 (success) */
-int main(void)                         
+/* main - Entry point */
+/* Return: Always 0 (success) */
+int main(void)
 {
 	int n;
 
