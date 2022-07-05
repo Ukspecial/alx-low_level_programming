f2b146cfe126ba08dae370cf191a9e524c3c171c
Uko Udo <donnywise.uk@gmail.com>
2022-07-05 16:07:41 +0000

    project 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Enty point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+void print_alphabet(void);
+
+int main(void)
+{
+	int x;
+
+	for (x = a; x <= z; a++)
+	{
+		putchar(x);
+		putchar(\n);
+	return (0);
