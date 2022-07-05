a2cbb28f10e613d9af80a305029228556bad071d
Uko Udo <donnywise.uk@gmail.com>
2022-07-05 17:01:18 +0000

    project 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - Enty point
+ * main - Entry point
+ *
+ * Description: 'the program's description'
  *
  * Return: Always 0 (Success)
  */
-
 
 void print_alphabet(void);
 
 int main(void)
 {
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
 	int x;
 
 	for (x = a; x <= z; a++)
-	{
-		putchar(x);
-		putchar(\n);
-	return (0);
+	putchar(x);
+	putchar(\n);
+}
