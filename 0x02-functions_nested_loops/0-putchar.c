4cd8fc78ff9a3b1264f68fe2639f428d0c3698ec
Uko Udo <donnywise.uk@gmail.com>
2022-07-05 13:00:35 +0000

    Printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print putchar, followed by new line
+ *
+ * return: 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Please read the program instruction here
